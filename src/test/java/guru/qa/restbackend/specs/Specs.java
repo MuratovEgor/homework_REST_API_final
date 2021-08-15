847c3b65bf20b95684cb79fb900a4724776573a2
Egor Muratov <egormuratov@yahoo.com>
2021-08-15 18:58:19 +0300

    created Specs

---
+++ src/test/java/guru/qa/restbackend/specs/Specs.java
@@ -0,0 +1,16 @@
+package guru.qa.restbackend.specs;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.with;
+
+public class Specs {
+    public static RequestSpecification booksRequestSpec = with()
+            .baseUri("http://localhost:8080")
+            .basePath("/books");
+
+    public static RequestSpecification authorsRequestSpec = with()
+            .baseUri("http://localhost:8080")
+            .basePath("/authors").contentType(ContentType.JSON);
+}
